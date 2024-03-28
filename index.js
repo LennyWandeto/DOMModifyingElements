3e86f88cd60fdc50293adf49196fce5a92fca0cc
Lenny Wandeto <lenny.wandeto@student.moringaschool.com>
2024-03-28 09:37:07 +0300

    updates index.js for completion

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Write your code here
+
+const main = document.getElementById("main")
+main.remove();
+
+let newHeader = document.createElement("h1")
+
+newHeader.setAttribute("id", "victory")
+
+newHeader.innerHTML = "Lenny is the champion"
